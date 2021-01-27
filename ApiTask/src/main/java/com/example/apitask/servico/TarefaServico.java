5897348bf49bd5ecb65aeefbf8cd4d03c684b25e
joao.calixto <joao.calixto@basis.com.br>
2021-01-27 20:52:59 -0300

    desafio api task concluido

---
+++ ApiTask/src/main/java/com/example/apitask/servico/TarefaServico.java
@@ -0,0 +1,15 @@
+package com.example.apitask.servico;
+
+import com.example.apitask.servico.dto.TarefaDTO;
+import com.example.apitask.servico.dto.TarefaListagemDTO;
+
+import java.util.List;
+
+public interface TarefaServico {
+
+    public List<TarefaListagemDTO> listar();
+
+    public TarefaListagemDTO salvar(TarefaDTO tarefaDTO);
+
+    public void deletar(Long id);
+}
---
+++ ApiTask/src/test/java/com/example/apitask/GtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
