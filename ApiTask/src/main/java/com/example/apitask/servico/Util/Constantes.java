f3d2b2a46504936f7c833f60376785a099a62961
joao.calixto <joao.calixto@basis.com.br>
2021-01-31 19:13:41 -0300

    fix

--- ApiTask/src/main/java/com/example/apitask/servico/Util/Constantes.java
+++ ApiTask/src/main/java/com/example/apitask/servico/Util/Constantes.java
@@ -2,6 +2,6 @@ package com.example.apitask.servico.Util;
 
 public class Constantes {
 
-    public static final String TAREFA_PENDENTE = "Pendente";
-    public static final String TAREFA_CONCLUIDA = "Concluída";
+    public static final String TAREFA_PENDENTE = "PENDENTE";
+    public static final String TAREFA_CONCLUIDA = "CONCLUÍDA";
 }
